8e6f1c6a84e6e239df4cc859e94e7244ea4e8a8a
Fernando Avalos <fernandoadev@protonmail.com>
2026-03-06 15:19:38 -0500

    Fix type

--- src/config.rs
+++ src/config.rs
@@ -11,5 +11,5 @@ pub const CAMERA_DELTA: f32 = 10.0;
 pub const GRID_COLOR: Color = Color::RGB(64, 64, 64);
 pub const CELL_COLOR: Color = Color::RGB(0, 255, 0);
 pub const FEEDBACK_COLOR: Color = Color::RGB(255, 255, 255);
-pub const QT_DIM: u32 = 15;
+pub const QT_DIM: usize = 15;
 pub const STEP: usize = 1;
