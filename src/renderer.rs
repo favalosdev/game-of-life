44e78ca74984b62543fa5eb3d1599c856b4c3d10
Fernando Avalos <fernandoadev@protonmail.com>
2026-03-17 20:30:07 -0500

    Nit

--- src/renderer.rs
+++ src/renderer.rs
@@ -20,6 +20,7 @@ macro_rules! rect(
 
 fn get_rect(camera: &Camera, x_raw: isize, y_raw: isize) -> Rect {
     let (xo_w, yo_w) = (x_raw, y_raw);
+
     let (xo_s, yo_s) = camera.from_world_coords((xo_w, yo_w));
     let (xf_s, yf_s) = camera.from_world_coords((xo_w + 1, yo_w + 1));
 
