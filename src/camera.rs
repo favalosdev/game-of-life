4d692282476cb2a9f825cec328409ba0ad9aa8c3
Fernando Avalos <fernandoadev@protonmail.com>
2026-03-23 18:31:12 -0500

    Add frac_zoom coordinate transformation

--- src/camera.rs
+++ src/camera.rs
@@ -1,25 +1,45 @@
+use crate::config::DEFAULT_ZOOM;
 use crate::quad_tree::WCoord;
 
 pub struct Camera {
     pub zoom: i32,
-    pub x: i32,
-    pub y: i32
+    pub frac_zoom: f32,
+    pub pos: (i32, i32)
 }
 
 impl Camera {
-    pub fn new(zoom: i32, x: i32, y: i32) -> Self {
-        Self { zoom, x, y }
+    pub fn new() -> Self {
+        Self {
+            zoom: DEFAULT_ZOOM,
+            frac_zoom: 1.0,
+            pos: (0, 0)
+        }
     }
 
-    pub fn from_world_coords(&self, (x_w, y_w): WCoord) -> (i32, i32) {
-        let x_s = ((x_w as i32) - self.x) * self.zoom;
-        let y_s = ((y_w as i32) - self.y) * self.zoom;
+    pub fn from_world_coords(&self, target: WCoord, frac: bool) -> (i32, i32) {
+        if frac {
+            self.from_world_coords_frac(target)
+        } else {
+            self.from_world_coords_whole(target)
+        }
+    } 
+
+    fn from_world_coords_whole(&self, (x_w, y_w): WCoord) -> (i32, i32) {
+        let x_s = ((x_w as i32) - self.pos.0) * self.zoom;
+        let y_s = ((y_w as i32) - self.pos.1) * self.zoom;
         (x_s, y_s)
     }
 
+    // Dirty solution but we can correct it later on with AI, hopefully
+    fn from_world_coords_frac(&self, (x_w, y_w): WCoord) -> (i32, i32) {
+        let x_s = ((x_w as i32) - self.pos.0) as f32 * self.frac_zoom;
+        let y_s = ((y_w as i32) - self.pos.1) as f32 * self.frac_zoom;
+        (x_s.floor() as i32, y_s.floor() as i32)
+    }
+
     pub fn from_screen_coords(&self, (x_s, y_s): (i32, i32)) -> WCoord {
-        let x_w = x_s.div_euclid(self.zoom) + self.x;
-        let y_w = y_s.div_euclid(self.zoom) + self.y;
+        let x_w = x_s.div_euclid(self.zoom) + self.pos.0;
+        let y_w = y_s.div_euclid(self.zoom) + self.pos.1;
         (x_w as isize, y_w as isize)
     }
 }
