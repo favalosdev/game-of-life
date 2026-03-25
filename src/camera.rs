9d6a62be717328d4fc434ba7d41340e48d82d384
Fernando Avalos <fernandoadev@protonmail.com>
2026-03-25 05:25:56 -0500

    Mix zoom function

--- src/camera.rs
+++ src/camera.rs
@@ -16,26 +16,12 @@ impl Camera {
         }
     }
 
-    pub fn from_world_coords(&self, target: WCoord, frac: bool) -> (i32, i32) {
-        if frac {
-            self.from_world_coords_frac(target)
-        } else {
-            self.from_world_coords_whole(target)
-        }
-    } 
-
-    fn from_world_coords_whole(&self, (x_w, y_w): WCoord) -> (i32, i32) {
-        let x_s = ((x_w as i32) - self.pos.0) * self.zoom;
-        let y_s = ((y_w as i32) - self.pos.1) * self.zoom;
-        (x_s, y_s)
-    }
-
-    // Dirty solution but we can correct it later on with AI, hopefully
-    fn from_world_coords_frac(&self, (x_w, y_w): WCoord) -> (i32, i32) {
-        let x_s = ((x_w as i32) - self.pos.0) as f32 * self.frac_zoom;
-        let y_s = ((y_w as i32) - self.pos.1) as f32 * self.frac_zoom;
+    pub fn from_world_coords(&self, (x_w, y_w): WCoord, frac: bool) -> (i32, i32) {
+        let zoom = if frac { self.frac_zoom } else { self.zoom as f32 };
+        let x_s = ((x_w as i32) - self.pos.0) as f32 * zoom;
+        let y_s = ((y_w as i32) - self.pos.1) as f32 * zoom; 
         (x_s.floor() as i32, y_s.floor() as i32)
-    }
+    } 
 
     pub fn from_screen_coords(&self, (x_s, y_s): (i32, i32)) -> WCoord {
         let x_w = x_s.div_euclid(self.zoom) + self.pos.0;
