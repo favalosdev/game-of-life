2630d2c30b9ed9fa1e2ad91cf7909b9a01258204
Fernando Avalos <fernandoadev@protonmail.com>
2026-04-07 12:47:45 -0500

    Fix unwind/rewind system

--- src/history.rs
+++ src/history.rs
@@ -7,8 +7,8 @@ pub struct History {
 }
 
 impl History {
-    pub fn new(capacity: usize, init_state: NodeId) -> Self {
-        let mut tape: VecDeque<NodeId> = VecDeque::with_capacity(capacity);
+    pub fn new(init_state: NodeId) -> Self {
+        let mut tape: VecDeque<NodeId> = VecDeque::new();
         tape.push_back(init_state);
 
         Self {
@@ -18,35 +18,37 @@ impl History {
     }
 
     pub fn unwind(&mut self) {
-        if self.pointer > 0 {
-            self.pointer -= 1;
-        }
+        self.pointer -= 1;
     }
 
     pub fn rewind(&mut self) {
-        if self.pointer + 1 < self.tape.len() {
-            self.pointer += 1;
-        }
+        self.pointer += 1;
+    }
+
+    pub fn can_rewind(&mut self) -> bool {
+        (self.tape.len() - 1) - self.pointer >= 1 
+    }
+
+    pub fn can_unwind(&mut self) -> bool {
+        self.pointer > 0
     }
 
     pub fn enqueue(&mut self, state: NodeId) {
         // Flush
-        if (self.tape.len() - 1) - self.pointer >= 1 {
+        if self.can_rewind() {
             self.tape.truncate(self.pointer + 1);
         }
 
-        let n = self.tape.len();
-
-        if n > 0 && n % self.tape.capacity() == 0 {
-            self.tape.pop_front();
-            self.unwind();
-        }
+        assert!(self.pointer == self.tape.len() - 1);
 
         self.tape.push_back(state);
-        self.rewind();
+        self.pointer += 1;
+        
+        assert!(self.pointer == self.tape.len() - 1);
     }
 
     pub fn state(&self) -> NodeId {
+        assert!(self.pointer < self.tape.len());
         self.tape[self.pointer]
     }
 }
